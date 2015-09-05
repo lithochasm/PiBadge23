e792103ef3566e847fe37f3e1d004690c10b14f8
toasty <toasty@mistakenot>
2015-09-05 04:16:15 +0000

    updated autorun to assume files are like the git repo was just cloned to your homedir...

--- config files/autorun.sh
+++ config files/autorun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while /bin/true ; do
-	for i in /home/pi/pi/videos/*.mov; do 
+	for i in ~/PiBadge/videos/*.mov; do 
 		mplayer -vo fbdev2:/dev/fb1 -x 480 -y 320 -zoom -framedrop -really-quiet $i; done
 done
